b1ebaef6e7b0fea7a7852b35c930110eebd48c98
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-02-21 16:33:34 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
+  # def new
+  # end
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
